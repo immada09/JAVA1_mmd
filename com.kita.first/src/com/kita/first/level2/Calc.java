8227def532484e408e9467a7b00190bf1ce740a5
immada09 <immada09@gmail.com>
2021-01-13 15:52:54 +0900

    210113

---
+++ com.kita.first/src/com/kita/first/level2/Calc.java
@@ -0,0 +1,75 @@
+package com.kita.first.level2;
+
+import java.util.Scanner;
+
+public class Calc {
+	//필드
+	static int n1 = 1;
+	final int N2 = 3;	//변하지않는 상수
+	
+	//생성자
+	
+	
+	//메소드
+	public static void main(String[] args) {
+		Calc myCalc = new Calc();
+		myCalc.powerOn();
+		
+		int num1 = myCalc.sum(2, 3);
+		System.out.println(num1);
+		
+		int num2 = myCalc.minus(3, 2);
+		System.out.println(num2);
+		
+		int num3 = myCalc.multiply(3, 2);
+		System.out.println(num3);
+		
+		int num4 = myCalc.divide(3, 2);
+		System.out.println(num4);
+	}
+	
+	public void powerOn(/*매개변수*/) {
+		System.out.println("전원을 켭니다.");
+	}
+	
+	//비void
+	public static int sum(int n1, int n2) {
+		int result = n1 + n2;
+		
+		return result;
+	}
+	
+	int minus(int n1, int n2) {
+		int result = n1 - n2;
+		
+		return result;
+	}
+	
+	int multiply(int n1, int n2) {
+		int result = n1 * n2;
+		
+		return result;
+	}
+	
+	double divide(int n1, int n2) {
+		double result = (double)n1 / n2;
+		
+		return result;
+	}
+	
+	
+	
+	public void powerOff() {
+		System.out.println("전원을 끕니다.");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- com.kita.first/src/com/kita/first/practice/Practice28.java
+++ com.kita.first/src/com/kita/first/practice/Practice28.java
@@ -1,5 +1,33 @@
 package com.kita.first.practice;
 
 public class Practice28 {
+	public static void main(String[] args) {
+		//국어합계:? 평균:?	00.0
+		//수학합계:? 평균:?
+		//영어합계:? 평균:?
+		//전체합계:? 평균:?
+		
+		int[][] arr = {
+				{90,100,88}, //국어
+				{34,99,45}, //수학
+				{89,65,74} //영어
+		};
+		
+		String[] clsArr = {"국어","수학","영어"};
+		int[] sumArr = new int[clsArr.length];
+		//각 과목의 성적 합해서 sumArr에 넣기
+		//각 과목 합계와 평균 출력하기
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
