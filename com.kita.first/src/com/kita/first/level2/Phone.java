7cbbfe500c3384567b8e98030e40ba1debba7f02
immada09 <immada09@gmail.com>
2021-01-15 15:52:30 +0900

    210115

--- com.kita.first/src/com/kita/first/level2/Phone.java
+++ com.kita.first/src/com/kita/first/level2/Phone.java
@@ -8,10 +8,36 @@ public class Phone {
 	int memory;
 	
 	//생성자: 필드(변수)를 초기화함
+	Phone () {} //기본생성자(메소드와 달리 리턴타입 없음)
+		
+	Phone(String name) {
+		this(name, "검은색", 200);
+	}
+	
+	Phone(String name, String color) {
+		this(name, color, 200);
+	}
+	
+	Phone(String name, String color, int memory) { //필드를 초기화함
+		this.name = name;
+		this.color = color;
+		this.memory = memory;
+	}
 	
 	//메소드
-	void sendMsg() {
+	//(리턴타입 or void) 메소드명(매개변수 선언) {
+	//	실행식
+	//	return 리턴타입변수
+	//}
+	String sendMsg(String str) {
+		String result = str;
 		
+		if("".equals(str)) {
+			String blank = "";
+			result = blank;
+		}
+		System.out.println(result);
+		return result;
 	}
 	
 	void call() {
