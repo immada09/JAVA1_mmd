ae5c7702c89e57238054f9bf743de608b80ba846
immada09 <immada09@gmail.com>
2021-01-26 15:48:48 +0900

    210126

--- com.kita.first/src/com/kita/first/level3/Test.java
+++ com.kita.first/src/com/kita/first/level3/Test.java
@@ -1,7 +1,16 @@
 package com.kita.first.level3;
 
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
+		ScanNum sn = new ScanNum();
+		String str = sn.inputNumString();
+		int result = Utils.parseStrToInt(str);
+		sn.printMsg(result);
+	}
+
+		
 //		ProGamer p1 = new ProGamer();
 //		Game game = new Game();
 //		p1.playGame(game);
@@ -33,32 +42,35 @@ public class Test {
 //		System.out.println(cat);
 //		System.out.println(animal == cat);
 		
-		System.out.println();
-	
-	Student minsu = new Student();
-	
-	for(int i=1;i<=5;i++) {
-		int result = minsu.readBooks();
-		switch(result) {
-		case 1:	//민수가 book1을 다 읽었을 경우
-			minsu.book1 = new ComicBook(3);
-			System.out.println("새 만화책을 가져왔다.");
-			break;
-		case 2:
-			minsu.book2 = new MajorTextBook(7);
-			System.out.println("새 전공책을 가져왔다.");
-			break;
-		case 3:
-			minsu.book3 = new Book(4);
-			System.out.println("새 책을 가져왔다.");
-			break;
-		}
-	System.out.println("-----------------");
-	
-	
-	
-	}
+//		System.out.println();
+//	
+//	Student minsu = new Student();
+//	
+//	for(int i=1;i<=5;i++) {
+//		int result = minsu.readBooks();
+//		switch(result) {
+//		case 1:	//민수가 book1을 다 읽었을 경우
+//			minsu.book1 = new ComicBook(3);
+//			System.out.println("새 만화책을 가져왔다.");
+//			break;
+//		case 2:
+//			minsu.book2 = new MajorTextBook(7);
+//			System.out.println("새 전공책을 가져왔다.");
+//			break;
+//		case 3:
+//			minsu.book3 = new Book(4);
+//			System.out.println("새 책을 가져왔다.");
+//			break;
+//		}
+//	System.out.println("-----------------");
+//	
+//	
+//	
+//	}
 
-	}
+		
+
+		
+	
 	
 }
